69eb43a23829a3b9b760798603e89b1bd2a7726c
Senne48 <157114914+Senne48@users.noreply.github.com>
2026-04-28 14:09:39 +0200

    Add files via upload

---
+++ Blinker.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <Blinker.h>
+
+// Klasse-implementatie
+Blinker::Blinker(unsigned int ledPin, unsigned long blinkInterval) {
+  _ledPin = ledPin;
+  _blinkInterval = blinkInterval;
+
+  _nu = 0;
+  _volgendeBlinkOm = _nu;
+}
+
+Blinker::~Blinker() {}
+
+int Blinker::begin() {
+  pinMode(_ledPin, OUTPUT);
+
+  return 0;
+};
+
+int Blinker::handle() {
+  int retVal = false;
+  _nu = millis();
+
+  if (_nu >= _volgendeBlinkOm) {
+    _volgendeBlinkOm = _nu + _blinkInterval;
+
+    digitalWrite(_ledPin, !digitalRead(_ledPin));
+
+    retVal = true;
+  }
+
+  return retVal;
+}
---
+++ Blinker.h
@@ -0,0 +1,20 @@
+#ifndef BLINKER_H
+    #define BLINKER_H
+    // Klassedefinitie
+    class Blinker{
+        private:
+        unsigned long _volgendeBlinkOm;
+        unsigned long _nu;
+        unsigned long _blinkInterval;
+        unsigned int  _ledPin;
+
+    public:
+        // Constructor
+        Blinker(unsigned int ledPin, unsigned long blinkInterval);
+        // Destructor
+        ~Blinker();
+
+    int begin();
+    int handle();
+};
+#endif
