5ac1b399afc09e2e3b3e00b069412045f806ee1f
Senne48 <157114914+Senne48@users.noreply.github.com>
2026-04-28 14:41:09 +0200

    Add startDelay parameter to Blinker constructor

--- Blinker.h
+++ Blinker.h
@@ -2,15 +2,16 @@
     #define BLINKER_H
     // Klassedefinitie
     class Blinker{
-        private:
+    private:
         unsigned long _volgendeBlinkOm;
         unsigned long _nu;
         unsigned long _blinkInterval;
+        unsigned long _startDelay;
         unsigned int  _ledPin;
 
     public:
         // Constructor
-        Blinker(unsigned int ledPin, unsigned long blinkInterval);
+        Blinker(unsigned int ledPin, unsigned long blinkInterval, const long startDelay = 0);
         // Destructor
         ~Blinker();
 
